49342c81eeb7e5333024f93dbe20c0949d2e27fd
dotrdp <rckydp1@gmail.com>
2025-06-09 22:56:16 -0600

    I think It could work?

--- configuration.nix
+++ configuration.nix
@@ -103,6 +103,8 @@
   programs.mtr.enable = true;
   programs.ssh.startAgent = true;
   services.openssh.enable = true;
+
+  system.copySystemConfiguration = true;
   # This option defines the first version of NixOS you have installed on this particular machine,
   # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
   #
