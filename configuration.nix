d8e04c59d669aba9e6cbbc54977423c53f4e4955
dotrdp <rckydp1@gmail.com>
2025-06-10 16:45:16 -0600

    OK FINALLY IT IS CLEAN AND WORKS;

--- configuration.nix
+++ configuration.nix
@@ -104,7 +104,6 @@
   programs.ssh.startAgent = true;
   services.openssh.enable = true;
 
-  system.copySystemConfiguration = true;
   # This option defines the first version of NixOS you have installed on this particular machine,
   # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
   #
