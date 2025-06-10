e534b89b9dc241fb6a6125b7469282a877b18078
dotrdp <rckydp1@gmail.com>
2025-06-10 07:42:49 -0600

    Further flake support

--- configuration.nix
+++ configuration.nix
@@ -36,8 +36,8 @@
 
   # DE
   services.xserver.enable = true;
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
+  services.displayManager.gdm.enable = true;
+  services.desktopManager.gnome.enable = true;
   
   # DISPLAYLINK DRIVER
   services.xserver.videoDrivers = [ "displaylink" "modesetting" ]; 
